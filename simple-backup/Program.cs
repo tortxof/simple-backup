d30b1e7d7bfbc29bc089a55505851b287a15aeb3
Daniel Jones <tortxof@gmail.com>
2018-03-08 12:01:31 -0500

    Remove unused using statements

--- simple-backup/Program.cs
+++ simple-backup/Program.cs
@@ -1,11 +1,7 @@
-using System;
+﻿using System;
 using System.IO;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Newtonsoft.Json;
 
 namespace simple_backup
